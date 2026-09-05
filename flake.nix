0a1d2876b59e2640dd0b6f1538584791ad9e0482
Aaron DeVera <aaronsdevera@protonmail.com>
2026-09-05 10:40:42 -0400

    bump release

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
         };
         klepto = rustPlatform.buildRustPackage {
           pname = "klepto";
-          version = "0.5.7";
+          version = "0.5.8";
           src = ./klepto;
           cargoLock = {
             lockFile = ./klepto/Cargo.lock;
